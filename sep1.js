7e515ecb79d43a4370e9a3bb3fcfc97b30870a44
Priyanka Mishra <71710239+DazzleProgrammer@users.noreply.github.com>
2023-10-10 21:43:03 +0530

    Add files via upload

---
+++ sep1.js
@@ -0,0 +1,69 @@
+//Q1.  What should come next 4, 6, 9,13 ? -> 15,12,18,17 -> Number Series
+
+// console.log(4 + 2)
+// console.log(6 + 3)
+// console.log(9 + 4)
+// console.log(13 + 5)
+
+//Q2.  N A T U R E = M A S U Q E => A U E are same => Pairing => M N, S T, Q R 
+//     M A S U Q E
+
+// F A M I N E => E A L I M E 
+
+
+// Q3. Find different Ear, Lungs, Liver, Intestine
+
+// Q4. M A D R A S    
+//     N B E S B T 
+
+//  B O M B A Y
+//  C P N C B Z
+
+
+// Q4. Find different Swimming, Diving, Sailing, Driving: Drvining
+
+
+// Q5. 8, 28, 116, 584, ?
+
+// 8 * 3 = 24 + 4 => 28
+// 28 * 4 = 112 + 4 => 116
+// 116 * 5 = 580 + 4 => 584
+// 584 * 6 = 3504 + 4 => 3508
+
+// Q6. Thermometer:Temperature::Glucometer:Blood Sugar
+
+// Q7. 2,1,1/2,1/4,1/8
+
+// @8. 3,6,8,16,18, ? = 36
+
+// @9. Dog:Rabbies::Mosquito:Malaria
+
+// @10. 3,2,7,6,11,? => 10
+
+// @11. Rahul 25yrs chotu than mummy. RB is 35 yrs chotu than mummy and born in 1964 .Rahul DOB ? 
+
+// => Rahul DOB + 10 = RB DOB = 1964 => 1954
+
+// @12 C U R L I N G   ::  N E T B A L L 
+// =>  D W U P N T N   ::  O G W F F R S
+
+// C D => U V W => R S T U => L M N O P => I J K L M N  => N O P Q R S T => G H I J K L M N 
+// N O => E F G => T U V W => B C D E F => A B C D E F 
+
+
+// @13 4386 = 43*2 = 86, 3264 = 32*2 = 64, 3774 = 37*2 = 74, 4869 = 48*2 = 96
+
+// @14 D E S T I N Y => W V H G R M B :: M A T H => N Z G S
+
+
+//  B o Y , M a N , Indian Railway , Electric Vehicle , DeW , High School 
+
+// @15 T I G E R => Q D F H S  
+ 
+//  T -> S , Q -> R => F -> G, H -> I
+
+
+// @16    174, 201, 230, 261, ? , 329 =>  201 - 174 = 27 :: 230 - 201 = 29 :: 261 - 230 = 31 :: 261 + 33 = 294 
+
+
+
